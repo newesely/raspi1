5ea6ece8e9a31931d646cf7b12fe77d24c54415a
newesely <c.newesely@gmail.com>
2018-06-24 17:13:22 +0200

    Sicherungskopie

---
+++ Hallo_Welt - Kopie.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#Example (Hallo Welt):
+import Tkinter
+
+tk = Tkinter.Tk()
+frame = Tkinter.Frame(tk, relief="ridge", borderwidth=2)
+tk.title("Mein Fenster")
+
+#Fullscrenmodus
+#tk.attributes('-fullscreen',True)
+#tk.attributes('-topmost',True)
+#tk.overrideredirect(True)
+
+#Fenstergroeße (X)x(y)
+
+tk.geometry('640x480') 
+frame.pack(fill="both",expand=1)
+label = Tkinter.Label(frame, text="Hallo Welt!")
+label.pack(expand=1)
+button = Tkinter.Button(frame,text="Beenden", command=tk.destroy)
+button.pack(side="bottom")
+
+tk.mainloop()
